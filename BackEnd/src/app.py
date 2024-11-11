3f8ed45afb592bc883861fd5335a76e182a3ae3b
ChanguaZzz <ggualdronarjona@gmail.com>
2024-11-11 13:52:56 +0100

    remove app.run

--- BackEnd/src/app.py
+++ BackEnd/src/app.py
@@ -259,7 +259,4 @@ with app.app_context():
     db.create_all()
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000, debug=True)
-
-if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=10000, debug=True)
