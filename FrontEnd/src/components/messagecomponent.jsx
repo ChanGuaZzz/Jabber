add62ee7b0836ee29d4722b7658e4c9d9e34ebe0
ChanguaZzz <ggualdronarjona@gmail.com>
2024-12-11 11:11:46 +0100

    justify

--- FrontEnd/src/components/messagecomponent.jsx
+++ FrontEnd/src/components/messagecomponent.jsx
@@ -18,7 +18,7 @@ function MessageComponent({ messageid, sender, message, time, isMe, userId }) {
 
       {viewUser && <ModalViewUser setViewUser={setViewUser} userId={userId} />}
       <div className={`${isMe ? "me" : "them"} chatmessage p-1  max-w-[70%] flex flex-col  `}>
-        <div className="flex ">
+        <div className="flex justify-between ">
           <p className={` break-words px-2 ${isMe ? "w-[95%]" : ""}`}>
             {!isMe && (
               <strong className="cursor-pointer" onClick={() => setViewUser(true)}>
