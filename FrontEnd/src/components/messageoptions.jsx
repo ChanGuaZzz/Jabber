70c4a0dac401b817f4eee6941f90281b9b7060dd
ChanguaZzz <ggualdronarjona@gmail.com>
2024-12-17 22:09:28 +0100

    add loading state to message options component; implement loading indicator during message edit and delete operations

--- FrontEnd/src/components/messageoptions.jsx
+++ FrontEnd/src/components/messageoptions.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
+import { set } from "date-fns";
 import { useState, useEffect } from "react";
+import Loading from "./loading";
 
 function MessageOptions({ setMessageOptions, messageId, currentContent }) {
   const [content, setContent] = useState("");
   const [edit, setEdit] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleEdit = () => {
     setContent(currentContent);
@@ -11,31 +14,42 @@ function MessageOptions({ setMessageOptions, messageId, currentContent }) {
   };
 
   const deletemessage = () => {
+    setLoading(true);
     axios
       .post(`${import.meta.env.VITE_API_URL}/api/messageoptions`, { messageId, option: "delete" }, { withCredentials: true })
       .then((res) => {
         console.log(res);
         setMessageOptions(false);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
+
       });
   };
 
   const editmessage = () => {
+    setLoading(true);
     axios
       .post(`${import.meta.env.VITE_API_URL}/api/messageoptions`, { messageId, option: "edit", content }, { withCredentials: true })
       .then((res) => {
         console.log(res);
         setMessageOptions(false);
+        setLoading(false);
+
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
+
       });
   };
 
   return (
+
     <div className="z-40 fixed top-0 backdrop-blur-[1px] left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+      {loading && ( <Loading /> )}
       <div className="bg-white flex flex-col w-[200px] rounded-md p-2">
         <button
           className="py-3 text-red-600 "
