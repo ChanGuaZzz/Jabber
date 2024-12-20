ab704395efd8a066d99ad9171cb459e53ecd0a07
ChanguaZzz <ggualdronarjona@gmail.com>
2024-12-20 23:05:10 +0100

    enhance jabber page loading state handling; adjust loading condition for improved user experience

--- FrontEnd/src/pages/jabber.jsx
+++ FrontEnd/src/pages/jabber.jsx
@@ -280,8 +280,8 @@ function Jabber() {
               <h2 className="text-shadow font-semibold">Chat in {currentRoom}</h2>
             </div>
             <div className="">
-              <div className="flex flex-col h-[85vh] pb-28  bg-chat rounded-md messages-container overflow-auto scrollbar-dark" ref={scrollRef}>
-                {loading ? (
+              <div className=" relative flex flex-col h-[85vh] pb-28  bg-chat rounded-md messages-container overflow-auto scrollbar-dark" ref={scrollRef}>
+                {!loading ? (
                   <div className="absolute bg-black w-full h-full bg-opacity-60 backdrop-blur-sm ">
                     <div className=" loadinganimation size-full flex justify-center items-center text-white">
                       <div className=" size-2 mx-1 bg-white rounded-full transition-all"></div>
