d988ec9d180f1e791862aecc839cd3729240b50a
ChanguaZzz <ggualdronarjona@gmail.com>
2024-12-20 21:15:32 +0100

    enhance loading component with logo and improved styling in Jabber page

--- FrontEnd/src/components/loading.jsx
+++ FrontEnd/src/components/loading.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "../styles/input.css";
+import logo from "../img/logo.png";
 
 function Loading() {
   return (
-    <div className="absolute bg-black size-full opacity-75">
-      <div className="flex items-center justify-center h-full w-full">
-        <div className="loader"></div>
+    <div className="absolute backdrop-blur-sm flex justify-center items-center bg-black size-full opacity-75">
+      <div className="flex flex-col items-center justify-center h-[20%] w-full">
+        <img src={logo} className="opacity-70" alt="Nothere" />
+        <div className=" loadinganimation size-full flex justify-center items-center text-white">
+          <div className=" size-2 mx-1 bg-white rounded-full transition-all"></div>
+          <div className="size-2 mx-1 bg-white rounded-full transition-all"></div>
+          <div className="size-2 mx-1 bg-white rounded-full transition-all"></div>
+        </div>
       </div>
     </div>
   );
